25526280591c181b7b6521ad161fe18e07d52a2b
JakeytheeClown <165510653+JakeytheeClown@users.noreply.github.com>
2025-07-01 22:47:32 -0400

    hi
    for the love of god this wait function better work

---
+++ fake grow a garden dupe script.lua
@@ -0,0 +1,7 @@
+print("Cheking Pet Inventory")
+print("Please wait up to 10 seconds for Pets to dupe")
+task.wait(10)
+if print then
+    warn("Unable to dupe please get your brain checked before thinking dupe scripts are real.")
+end
+--this shit probably dont work
